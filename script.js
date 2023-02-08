67f8fae9a9e46d7716457dfdab50ba794aa8fba2
Jake-Ashkenase <116736922+Jake-Ashkenase@users.noreply.github.com>
2023-02-08 09:31:11 -0500

    final push

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+
+// function to create a buttom that changes our page to dark mode
+// text becomes light color and background becomes dark color
 function dark_mode() {
+	// starts with normal colors "mode"
 	let body = document.body; 
 	body.classList.toggle("mode");
 
+	// switches to second set of colors "button_mode" when button is hit
 	let button = document.getElementById("button");
-	button.classList.toggle("button_mode")
+	button.classList.toggle("button_mode");
 }
